0e9f8d8c03fa7ede668a4da3347ec6edacb8664c
akbarb24 <akbar.binaji@gmail.com>
2020-06-03 16:37:10 +0800

    'init'

---
+++ src/test/java/com/mitrais/chipper/temankondangan/backend/authservice/AuthServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mitrais.chipper.temankondangan.backend.authservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AuthServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
