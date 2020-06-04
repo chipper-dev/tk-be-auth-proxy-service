d6113f2d4d72bb87b13aa916cec2113269ed40d9
akbarb24 <akbar.binaji@gmail.com>
2020-06-04 16:51:23 +0800

    config zuul n eureka server

--- src/main/java/com/mitrais/chipper/temankondangan/backend/authservice/AuthServiceApplication.java
+++ src/main/java/com/mitrais/chipper/temankondangan/backend/authservice/AuthServiceApplication.java
@@ -2,7 +2,13 @@ package com.mitrais.chipper.temankondangan.backend.authservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
+@EnableZuulProxy //act as Zuul proxy
+@EnableEurekaServer//for making this application as eureka server
+@EnableFeignClients
 @SpringBootApplication
 public class AuthServiceApplication {
 
